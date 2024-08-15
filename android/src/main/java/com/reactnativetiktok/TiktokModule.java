200c62a47ec8daad64865899cae847b0fce94b12
Acacius Shun <13795923+AcaciusShun@users.noreply.github.com>
2024-08-15 18:53:54 +0800

    Update TiktokModule.java

--- android/src/main/java/com/reactnativetiktok/TiktokModule.java
+++ android/src/main/java/com/reactnativetiktok/TiktokModule.java
@@ -75,7 +75,8 @@ public class TiktokModule extends ReactContextBaseJavaModule {
     public void auth(Callback callBack) {
       TikTokOpenApi tiktokOpenApi = TikTokOpenApiFactory.create(getCurrentActivity());
       Authorization.Request request = new Authorization.Request();
-      request.scope = "user.info.basic";
+      // request.scope = "user.info.basic";
+      request.scope = "user.info.basic,user.info.stats,user.info.profile";
       request.callerLocalEntry = "com.reactnativetiktok.TikTokEntryActivity";
       tiktokOpenApi.authorize(request);
     }
